e7b0f5ba9c9858b6a00e90ea92e2a0b184e32fe2
Yash Gautam <117110589+yash-geek@users.noreply.github.com>
2024-10-06 10:20:02 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -124,7 +124,7 @@ async function displayAlbums() {
 
     Array.from(document.getElementsByClassName("card")).forEach(e => {
         e.addEventListener("click", async item => {
-            songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`)
+            songs = await getSongs(`/songs/${item.currentTarget.dataset.folder}`)
             playMusic(songs[0])
 
 

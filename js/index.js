241f5f96d2e3bf46eec78c9696c8e58acb5fbf46
Yash Gautam <117110589+yash-geek@users.noreply.github.com>
2024-12-14 05:06:36 +0000

    adjusting path 3

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const cards = document.querySelectorAll('.card');
-const baseURL = "https://yash-geek.github.io/Purrfect-Music-Player";
+const baseURL = "";
 // fetch(`${baseURL}mySongs`)
 cards.forEach(card => {
     const playButton = card.querySelector('.playbutton');
